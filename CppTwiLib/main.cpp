5d9a22bdc697a4d8f848c6835f9aea36a0777093
Hiroki Yamasita <sayahamitt@gmail.com>
2014-09-11 01:51:08 +0900

    Initial Commit

---
+++ CppTwiLib/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CppTwiLib
+//
+//  Created by Hiroki Yamasita on 2014/09/11.
+//  Copyright (c) 2014年 Hiroki Yamasita. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
