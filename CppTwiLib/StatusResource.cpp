502291a747794ea5258b04ca7773850393106f36
Hiroki Yamasita <sayahamitt@gmail.com>
2014-10-18 15:55:12 +0900

    StatusResourceクラスにAPIメソッドを追加した。
    TwitterAPIのstatus/以下のAPIを利用する為のメソッドを追加した。
    但し、以下のAPIは未実装。
    	POST statuses/update_with_media
    	GET statuses/oembed
    	GET statuses/lookup

--- CppTwiLib/StatusResource.cpp
+++ CppTwiLib/StatusResource.cpp
@@ -22,6 +22,15 @@ StatusResource::StatusResource(Twiauth certifyer):Twistream(certifyer){
     APIRESOURCENAME = "statuses";
 }
 
+std::vector<tweet> StatusResource::mentions_timeline(std::map<std::string,std::string> paramaters){
+    std::vector<tweet> timeline;
+    
+    requesttoTwitter(GET,"mentions_timeline.json", paramaters);
+    
+    createObjectsArray(timeline);
+    return timeline;
+}
+
 std::vector<tweet> StatusResource::hometimeline(std::map<std::string,std::string> paramaters){
     std::vector<tweet> timeline;
     std::string http_header;
@@ -46,6 +55,49 @@ std::vector<tweet> StatusResource::user_timeline(std::string screenname,std::map
     return timeline;
 }
 
+std::vector<tweet> StatusResource::retweets_of_me(std::map<std::string,std::string> paramaters){
+    std::vector<tweet> timeline;
+    
+    requesttoTwitter(GET,"retweets_of_me.json", paramaters);
+    
+    createObjectsArray(timeline);
+    return timeline;
+}
+
+std::vector<tweet> StatusResource::retweets_id(int64_t ID,std::map<std::string,std::string> paramaters){
+    std::vector<tweet> timeline;
+    
+    paramaters.erase("id");
+    
+    requesttoTwitter(GET,"retweets/"+std::to_string(ID)+".json", paramaters);
+    
+    createObjectsArray(timeline);
+    return timeline;
+}
+
+tweet StatusResource::show_id(int64_t ID,std::map<std::string, std::string> paramaters){
+    tweet status;
+    
+    paramaters["id"]=std::to_string(ID);
+    
+    requesttoTwitter(GET, "show.json",paramaters);
+    
+    if (response.is<picojson::object>()){
+        status = tweet(response.get<picojson::object>());
+    }
+    return status;
+}
+
+tweet StatusResource::destory_id(int64_t ID){
+    tweet status;
+    
+    requesttoTwitter(POST, "destroy/"+std::to_string(ID)+".json");
+    
+    if (response.is<picojson::object>()){
+        status = tweet(response.get<picojson::object>());
+    }
+    return status;
+}
 
 tweet StatusResource::update(std::string poststr){
     std::map<std::string, std::string> param;
@@ -66,3 +118,43 @@ tweet StatusResource::update(std::string poststr){
     
     return posted;
 }
+
+tweet StatusResource::retweet_id(int64_t ID){
+    tweet status;
+    
+    requesttoTwitter(POST, "retweet/"+std::to_string(ID)+".json");
+    
+    if (response.is<picojson::object>()){
+        status = tweet(response.get<picojson::object>());
+    }
+    return status;
+}
+
+std::vector<int64_t> StatusResource::retweeters_ids(int64_t ID){
+    std::map<std::string, std::string> param;
+    std::vector<int64_t> retweeters;
+    picojson::array ids;
+    
+    std::string nextCoursor;
+    int counter=0;
+    do{
+        requesttoTwitter(GET, "retweeters/ids.json",param);
+        if (!response.is<picojson::object>()) {
+            return retweeters;
+        }
+        
+        picojson::object resObj = response.get<picojson::object>();
+        nextCoursor = resObj["previous_cursor_str"].get<std::string>();
+        
+        ids = resObj["ids"].get<picojson::array>();
+        
+        for (picojson::array::iterator i = ids.begin(); i != ids.end(); i++) {
+            retweeters[counter++] = (*i).get<int64_t>();
+        }
+        
+        param["cursor"]=nextCoursor;
+    }while (nextCoursor != "0");
+    
+    return retweeters;
+}
+
--- CppTwiLib/StatusResource.h
+++ CppTwiLib/StatusResource.h
@@ -18,9 +18,17 @@ public:
     StatusResource(std::string c_key, std::string c_sec, std::string token, std::string token_sec);
     StatusResource(Twiauth certifyer);
     
+    std::vector<tweet> mentions_timeline(std::map<std::string, std::string> paramaters);
     std::vector<tweet> hometimeline(std::map<std::string, std::string> paramaters);
     std::vector<tweet> user_timeline(std::string screenname, std::map<std::string, std::string> paramaters);
+    std::vector<tweet> retweets_of_me(std::map<std::string, std::string> paramaters);
+    std::vector<tweet> retweets_id(int64_t ID,std::map<std::string, std::string> paramaters);
+    tweet show_id(int64_t ID,std::map<std::string, std::string> paramaters);
+    tweet destory_id(int64_t ID);
     tweet update(std::string tweet);
+    tweet retweet_id(int64_t ID);
+    std::vector<int64_t> retweeters_ids(int64_t ID);
+
 };
 
 #endif /* defined(__CppTwiLib__StatusResource__) */
