40553957af80cd3a29a807c876f95a925bf7a34b
Hiroki Yamasita <sayahamitt@gmail.com>
2014-10-19 13:38:18 +0900

    UsersResourceクラスにメソッドとコンストラクタを追加。
    UsersResourceクラスにTwiauthクラスのインスタンスを引数にとって初期化できるコンストラクタを追加した。
    UsersResourceクラスにapi.twitter.com/1.1/users/ 以下のAPIを利用するメソッドを追加した。但し、以下のメソッドは実装していない。
    GET users/suggestions/:slug
    GET users/suggestions
    GET users/suggestions/:slug/members

--- CppTwiLib/UsersResource.cpp
+++ CppTwiLib/UsersResource.cpp
@@ -16,25 +16,68 @@ UsersResource::UsersResource(std::string c_key,std::string c_sec, std::string to
     APIRESOURCENAME = "users";
 }
 
-user UsersResource::users_lookup(std::map<std::string, std::string> parameters){
-    std::string http_header;
+UsersResource::UsersResource(Twiauth certifyer):Twistream(certifyer){
+    APIRESOURCENAME = "users";
+}
+
+std::vector<user> UsersResource::lookup(std::vector<std::string>& namelist){
+    std::map<std::string, std::string> parameters;
+    std::string unitedlist;
+    std::vector<user> userlist;
     
-    http_header = requesttoTwitter(GET,"lookup.json", parameters);
-    picojson::object strip;
+    for (std::vector<std::string>::iterator i=namelist.begin(); i != namelist.end(); i++) {
+        unitedlist += (*i)+",";
+    }unitedlist.erase(unitedlist.end()-1);
+    
+    parameters["screen_name"]=unitedlist;
+    
+    requesttoTwitter(GET,"lookup.json", parameters);
+    
+    createObjectsArray(userlist);
+    
+    return userlist;
+}
+
+user UsersResource::show_id(int64_t ID,std::map<std::string, std::string> parameters){
     user account;
-    if (response.is<picojson::array>()){
-        picojson::array& timeline = response.get<picojson::array>();
-        for (picojson::array::iterator i = timeline.begin(); i != timeline.end(); i++) {
-            //std::cout<<"\njson is array"<<std::endl;
-            strip = (*i).get<picojson::object>();
-        }
-        account = user(strip);
-    }else{
-        strip = response.get<picojson::object>();
-        account = user(strip);
-    }
-    //std::cout<<strResponse<<std::endl;
-    //std::string createdat;
     
+    parameters["id"]=std::to_string(ID);
+    
+    requesttoTwitter(GET, "show.json",parameters);
+    
+    if (response.is<picojson::object>()){
+        account = user(response.get<picojson::object>());
+    }
     return account;
+}
+
+std::vector<user> UsersResource::search(std::map<std::string, std::string> parameters){
+    std::vector<user> userlist;
+    
+    requesttoTwitter(GET,"search.json", parameters);
+    createObjectsArray(userlist);
+
+    return userlist;
+}
+
+std::vector<user> UsersResource::contributees(std::string screenname,std::map<std::string, std::string> parameters){
+    std::vector<user> userlist;
+    
+    parameters["screen_name"]=screenname;
+    
+    requesttoTwitter(GET,"contributees.json", parameters);
+    createObjectsArray(userlist);
+    
+    return userlist;
+}
+
+std::vector<user> UsersResource::contributors(std::string screenname,std::map<std::string, std::string> parameters){
+    std::vector<user> userlist;
+    
+    parameters["screen_name"]=screenname;
+    
+    requesttoTwitter(GET,"contributors.json", parameters);
+    createObjectsArray(userlist);
+    
+    return userlist;
 }
--- CppTwiLib/UsersResource.h
+++ CppTwiLib/UsersResource.h
@@ -15,7 +15,13 @@ class UsersResource : public Twistream{
 public:
     UsersResource(std::string c_key, std::string c_sec);
     UsersResource(std::string c_key, std::string c_sec, std::string token, std::string token_sec);
+    UsersResource(Twiauth certifyer);
     
-    user users_lookup(std::map<std::string, std::string> parameters);
+    std::vector<user> lookup(std::vector<std::string>& namelist);
+    user show_id(int64_t ID,std::map<std::string, std::string> parameters);
+    std::vector<user> search(std::map<std::string, std::string> parameters);
+    std::vector<user> contributees(std::string screenname,std::map<std::string, std::string> parameters);
+    std::vector<user> contributors(std::string screenname,std::map<std::string, std::string> parameters);
+
 };
 #endif /* defined(__CppTwiLib__Users__) */
