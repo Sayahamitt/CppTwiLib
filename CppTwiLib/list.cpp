e7ad27c6f40933f1931ecba2e6c9d0ec2c3fa676
Hiroki Yamasita <sayahamitt@gmail.com>
2014-10-19 14:44:10 +0900

    listクラスを作成した。
    listクラスを作成し、ひと通りのデータ取得メソッドを実装した。

---
+++ CppTwiLib/list.cpp
@@ -0,0 +1,100 @@
+//
+//  list.cpp
+//  CppTwiLib
+//
+//  Created by Hiroki Yamasita on 2014/10/19.
+//  Copyright (c) 2014年 Hiroki Yamasita. All rights reserved.
+//
+
+#include "list.h"
+#include "user.h"
+
+list::list(picojson::object response):Objects(response){
+}
+
+list::list():Objects(){};
+
+list::~list(){}
+
+bool list::getCreatedat(std::string& destination){
+    return getValuefromResponse<std::string>("created_at",destination);
+}
+
+bool list::getID(int64_t& destination){
+    bool res;
+    double temp;
+    
+    res = getValuefromResponse<double>("id",temp);
+    destination = (int64_t)temp;
+    
+    return res;
+}
+
+bool list::getIDstr(std::string &destination){
+    return getValuefromResponse<std::string>("id_str", destination);
+}
+
+bool list::getSubscriberCount(int& destination){
+    bool res;
+    double temp;
+    
+    res = getValuefromResponse<double>("subscriber_count",temp);
+    destination = (int)temp;
+    
+    return res;
+}
+
+bool list::getMemberCount(int& destination){
+    bool res;
+    double temp;
+    
+    res = getValuefromResponse<double>("member_count",temp);
+    destination = (int)temp;
+    
+    return res;
+}
+
+bool list::isPublic(bool& destination){
+    bool res;
+    std::string temp;
+    
+    res = getValuefromResponse<std::string>("mode",temp);
+    
+    if (temp == "public") {
+        destination = true;
+    }else{
+        destination = false;
+    }
+    return res;
+}
+
+bool list::getName(std::string &destination){
+    return getValuefromResponse<std::string>("name", destination);
+}
+
+bool list::getSlug(std::string &destination){
+    return getValuefromResponse<std::string>("slug", destination);
+}
+
+bool list::getFullName(std::string &destination){
+    return getValuefromResponse<std::string>("full_name", destination);
+}
+
+bool list::getDescription(std::string &destination){
+    return getValuefromResponse<std::string>("description", destination);
+}
+
+bool list::isFollowing(bool& destination){
+    return getValuefromResponse<bool>("description", destination);
+}
+
+bool list::getUser(user& destination){
+    picojson::object temp;
+    bool res;
+    
+    res = getValuefromResponse<picojson::object>("user", temp);
+    
+    user account(temp);
+    destination = account;
+    return res;
+}
---
+++ CppTwiLib/list.h
@@ -0,0 +1,39 @@
+//
+//  list.h
+//  CppTwiLib
+//
+//  Created by Hiroki Yamasita on 2014/10/19.
+//  Copyright (c) 2014年 Hiroki Yamasita. All rights reserved.
+//
+
+#ifndef __CppTwiLib__list__
+#define __CppTwiLib__list__
+
+#include <string>
+
+#include "objects.h"
+class user;
+
+class list : public Objects{
+protected:
+    
+public:
+    list(picojson::object response);
+    list();
+    ~list();
+    
+    bool getCreatedat(std::string& destination);
+    bool getID(int64_t& destination);
+    bool getIDstr(std::string& destination);
+    bool getSubscriberCount(int& destination);
+    bool getMemberCount(int& destination);
+    bool isPublic(bool& destination);
+    bool getName(std::string& destination);
+    bool getSlug(std::string& destination);
+    bool getFullName(std::string& destination);
+    bool getDescription(std::string& destination);
+    bool getUser(user& destination);
+    bool isFollowing(bool& destination);
+};
+
+#endif /* defined(__CppTwiLib__list__) */
